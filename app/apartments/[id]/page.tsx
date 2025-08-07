4aa2eacc5e6a04c34bcda775dfc837e0a057167f
Andrei Florea <floreaalexandru146@gmail.com>
2025-08-07 16:24:35 +0200

    FIX

--- app/apartments/[id]/page.tsx
+++ app/apartments/[id]/page.tsx
@@ -2,7 +2,13 @@
 
 import {ApartmentDetail} from "@/components/apartment-detail"
 
-export default function ApartmentPage({params}: {params: {id: string}}) {
+type Props = {
+  params: {
+    id: string
+  }
+}
+
+export default function ApartmentPage({params}: Props) {
   return (
     <main>
       <ApartmentDetail apartmentId={params.id} />
