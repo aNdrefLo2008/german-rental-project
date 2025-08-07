8645cfa4ed2425d55f00e1143803adebf66547ff
Andrei Florea <floreaalexandru146@gmail.com>
2025-08-07 16:19:53 +0200

    Fix

--- app/apartments/[id]/page.tsx
+++ app/apartments/[id]/page.tsx
@@ -3,5 +3,9 @@
 import {ApartmentDetail} from "@/components/apartment-detail"
 
 export default function ApartmentPage({params}: {params: {id: string}}) {
-  return <ApartmentDetail apartmentId={params.id} />
+  return (
+    <main>
+      <ApartmentDetail apartmentId={params.id} />
+    </main>
+  )
 }
