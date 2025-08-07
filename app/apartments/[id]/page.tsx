6dd9e475b94c44135cb00c28019f88f4063dcb3f
Andrei Florea <floreaalexandru146@gmail.com>
2025-08-07 16:31:41 +0200

    Same Bug

--- app/apartments/[id]/page.tsx
+++ app/apartments/[id]/page.tsx
@@ -2,16 +2,15 @@
 
 import {ApartmentDetail} from "@/components/apartment-detail"
 
-type Props = {
-  params: {
-    id: string
-  }
+interface Props {
+  params: Promise<{id: string}>
+  // optionally include searchParams if you use it
+  searchParams?: Promise<{[key: string]: string | string[] | undefined}>
 }
 
-export default function ApartmentPage({params}: Props) {
-  return (
-    <main>
-      <ApartmentDetail apartmentId={params.id} />
-    </main>
-  )
+export default async function ApartmentPage({params}: Props) {
+  const resolvedParams = await params
+  const id = resolvedParams.id
+
+  return <ApartmentDetail apartmentId={id} />
 }
