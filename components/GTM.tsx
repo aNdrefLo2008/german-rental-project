315dc0132495648f48a2a4d220a041a2f1b6f754
Andrei Florea <floreaalexandru146@gmail.com>
2025-08-18 18:55:58 +0200

    Tag Assistant Integration

--- components/GTM.tsx
+++ components/GTM.tsx
@@ -48,33 +48,31 @@ export function GTM() {
         `}
       </Script>
 
-      {consented && (
-        <>
-          {/* Consent granted nach Opt-In */}
-          <Script id='consent-grant' strategy='beforeInteractive'>
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('consent', 'update', {
-                ad_storage: 'granted',
-                ad_user_data: 'granted',
-                ad_personalization: 'granted',
-                analytics_storage: 'granted'
-              });
-            `}
-          </Script>
+      {/* GTM Container wird IMMER geladen */}
+      <Script id='gtm' strategy='afterInteractive'>
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${gtmId}');
+        `}
+      </Script>
 
-          {/* GTM laden */}
-          <Script id='gtm' strategy='afterInteractive'>
-            {`
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${gtmId}');
-            `}
-          </Script>
-        </>
+      {/* Consent granted nach Opt-In */}
+      {consented && (
+        <Script id='consent-grant' strategy='beforeInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('consent', 'update', {
+              ad_storage: 'granted',
+              ad_user_data: 'granted',
+              ad_personalization: 'granted',
+              analytics_storage: 'granted'
+            });
+          `}
+        </Script>
       )}
     </>
   )
@@ -83,10 +81,13 @@ export function GTM() {
 export function GTMNoScript() {
   const [consented, setConsented] = useState(false)
   const gtmId = process.env.NEXT_PUBLIC_GTM_ID
+
   useEffect(() => {
     setConsented(hasConsent())
   }, [])
-  if (!gtmId || !consented) return null
+
+  if (!gtmId) return null
+
   return (
     <noscript>
       <iframe
