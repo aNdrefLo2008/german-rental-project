c4413652bccbf47519ac09fd243d33ed00c68710
Andrei Florea <floreaalexandru146@gmail.com>
2025-09-01 17:16:31 +0200

    Start Header IMG

--- components/header.tsx
+++ components/header.tsx
@@ -23,7 +23,9 @@ export function Header() {
     <header className='sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
       <div className='container mx-auto px-4'>
         <div className='flex h-16 items-center justify-between'>
-          <Link href='/' className='flex items-start'>
+          <Link
+            href='/'
+            className='flex items-center sm:justify-center justify-start'>
             <Image
               src='/logo.svg'
               alt='Ferienwohnungen Gera'
