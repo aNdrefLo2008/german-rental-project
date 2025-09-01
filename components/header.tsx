bdb0d8849cd57299ba30762529fef0d904834c7c
Andrei Florea <floreaalexandru146@gmail.com>
2025-09-01 17:12:35 +0200

    Small Tweaks Header Logo

--- components/header.tsx
+++ components/header.tsx
@@ -23,14 +23,14 @@ export function Header() {
     <header className='sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
       <div className='container mx-auto px-4'>
         <div className='flex h-16 items-center justify-between'>
-          <Link href='/' className='flex items-center'>
+          <Link href='/' className='flex items-start'>
             <Image
               src='/logo.svg'
               alt='Ferienwohnungen Gera'
               width={40}
               height={40}
               priority
-              className='w-full h-10 object-contain'
+              className='w-full h-8 sm:h-10 object-contain'
             />
           </Link>
 
