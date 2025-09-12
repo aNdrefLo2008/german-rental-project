742f2f7f71e2efaa130298d531e3961077a608fd
Andrei Florea <floreaalexandru146@gmail.com>
2025-09-12 17:10:20 +0200

    FIX BUGS

--- components/about.tsx
+++ components/about.tsx
@@ -5,8 +5,7 @@ import {upsellQuery, reviewsQuery} from "@/sanity/lib/queries"
 import AboutClient from "./AboutClient"
 
 export default async function About() {
-  const upsell = await client.fetch(upsellQuery)
   const reviews = await client.fetch(reviewsQuery)
 
-  return <AboutClient upsell={upsell} reviews={reviews} />
+  return <AboutClient reviews={reviews} />
 }
