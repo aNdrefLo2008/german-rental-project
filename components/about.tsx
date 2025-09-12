df3edf6ec25e311796e68c41776a0c6f708028df
Andrei Florea <floreaalexandru146@gmail.com>
2025-09-12 17:11:58 +0200

    FIX BUGS

--- components/about.tsx
+++ components/about.tsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import {client} from "@/sanity/lib/client"
-import {upsellQuery, reviewsQuery} from "@/sanity/lib/queries"
+import {reviewsQuery} from "@/sanity/lib/queries"
 import AboutClient from "./AboutClient"
 
 export default async function About() {
